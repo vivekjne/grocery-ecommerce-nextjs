7dc833c5fb3bb12b4fc047f6290e2acc25dc1895
vaisakh.krishnan <vaisakh.krishnan@litmus7.com>
2020-09-02 11:59:02 +0530

    added primary button

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,31 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button'
-
+import PrimaryButton from './components/utils/PrimaryButton';
+import { ThemeProvider } from '@material-ui/core';
+import {defaultTheme} from '../helper/Theme';
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-      <Button variant="contained" color="primary">
-        <Typography>Button</Typography>
-      </Button>
-      </Box>
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Buttons example
+          </Typography>
+          <PrimaryButton color="primary">Primary Button</PrimaryButton>
+          <PrimaryButton color="secondary">Secondary Button</PrimaryButton>
+
+          <PrimaryButton color="primary" variant="outlined">Outlined Primary Button</PrimaryButton>
+          <PrimaryButton color="secondary" variant="outlined">Outlined Secondary Button</PrimaryButton>
+
+          <PrimaryButton color="primary" disabled>Disabled Primary Button</PrimaryButton>
+          <PrimaryButton color="primary" variant="outlined" disabled>Outlined Primary Button</PrimaryButton>
+
+          <PrimaryButton color="secondary" disabled>Disabled Secondary Button</PrimaryButton>
+          <PrimaryButton color="secondary" variant="outlined" disabled>Disabled Outlined Secondary Button</PrimaryButton>
+
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
